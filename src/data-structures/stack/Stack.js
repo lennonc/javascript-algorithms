1b83204411c5f0145156149b1998f0869affc15d
Lennon Chimbumu <lennonc@users.noreply.github.com>
2019-04-02 18:27:25 +0200

    Add the Stack

---
+++ src/data-structures/stack/Stack.js
@@ -0,0 +1,55 @@
+import LinkedList from '../linked-list/LinkedList';
+
+export default class Stack {
+  constructor() {
+    this.linkedList = new LinkedList();
+  }
+
+  isEmpty() {
+    return !this.linkedList.head;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    return this.linkedList.head.value;
+  }
+
+  /**
+   * Put a value on top of the stack
+   *
+   * @param {*} value
+   */
+  push(value) {
+    this.linkedList.prepend(value);
+  }
+
+  /**
+   * Get the value from the top of the list
+   */
+  pop() {
+    const removedHead = this.linkedList.deleteHead();
+    return removedHead ? removedHead.value : null;
+  }
+
+  /**
+   * @returns { *[]}
+   */
+  toArray() {
+    return this
+      .linkedList
+      .toArray()
+      .map(node => node.value);
+  }
+
+  /**
+   *
+   * @param {function} callback
+   * @returns {string}
+   */
+  toString(callback) {
+    return this.linkedList.toString(callback);
+  }
+}
